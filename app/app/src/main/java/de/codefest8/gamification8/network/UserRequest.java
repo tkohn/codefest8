c8bda6c4bcbc878de734d49f63afee254a7fb152
Alexander Devaykin <xaxellx@gmail.com>
2015-03-07 16:29:32 +0100

    Small refactorings.

--- app/app/src/main/java/de/codefest8/gamification8/network/UserRequest.java
+++ app/app/src/main/java/de/codefest8/gamification8/network/UserRequest.java
@@ -3,16 +3,12 @@ package de.codefest8.gamification8.network;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.apache.http.Header;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import de.codefest8.gamification8.UserMessagesHandler;
 
 public class UserRequest {
     private static final String URL_GENERAL = "/users/";
-    private JSONObject response;
-    private boolean isFinished;
-    private boolean isSucceed;
 
     public void requestGeneralInfo(int userId) {
         String url = URL_GENERAL+String.valueOf(userId);
@@ -36,17 +32,5 @@ public class UserRequest {
                 }
             }
         });
-    }
-
-    public boolean getIsFinished() {
-        return this.isFinished;
-    }
-
-    public boolean getIsSucceed() {
-        return this.isSucceed;
-    }
-
-    public JSONObject getResponse() {
-        return this.response;
     }
 }
