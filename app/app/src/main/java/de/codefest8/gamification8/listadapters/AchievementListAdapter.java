5159afebc96c1b05c61ab3173b68b2a71d0409a8
lukegv <lu.koerfer@googlemail.com>
2015-03-08 06:12:47 +0100

    Fixed ActionBarTitle and In-App-Navigation

--- app/app/src/main/java/de/codefest8/gamification8/listadapters/AchievementListAdapter.java
+++ app/app/src/main/java/de/codefest8/gamification8/listadapters/AchievementListAdapter.java
@@ -30,7 +30,10 @@ public class AchievementListAdapter extends ArrayAdapter<AchievementDTO> {
     {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.element_achievementlist, parent, false);
-        TextView textView = (TextView) view.findViewById(R.id.label);
+        TextView labelView = (TextView) view.findViewById(R.id.label);
+        TextView descrView = (TextView) view.findViewById(R.id.description);
+        labelView.setText(values[position].getName());
+        descrView.setText(Integer.toString(values[position].getPoints()) + " points achieved in this achievement!");
         return view;
     }
 
