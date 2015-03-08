fed5f467709c693eccabe6203548fda490b73e97
Kevin_Laptop <age.wc3@gmail.com>
2015-03-08 06:18:45 +0100

    Fixes for map. Doesn't crash on abort anymore.

--- app/app/src/main/java/de/codefest8/gamification8/fragments/TrackDetailFragment.java
+++ app/app/src/main/java/de/codefest8/gamification8/fragments/TrackDetailFragment.java
@@ -340,36 +340,35 @@ public class TrackDetailFragment extends Fragment  {
                 t.scheduleAtFixedRate(new TimerTask() {
                     @Override
                     public void run() {
-                        getActivity().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                i++;
+                        if(getActivity() != null) {
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    i++;
 
-                                if (i < points.size() - 1) {
-                                    LatLng cur = points.get(i);
-                                    LatLng nxt = points.get(i + 1);
-                                    Float heading = (float)computeHeading(cur, nxt);
-                                    CameraPosition pos = new CameraPosition.Builder().target(cur).bearing(heading).tilt(45).zoom(16).build();
-                                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(pos));
-                                    if (oldMarker != null)
-                                    {
-                                        oldMarker.remove();
+                                    if (i < points.size() - 1) {
+                                        LatLng cur = points.get(i);
+                                        LatLng nxt = points.get(i + 1);
+                                        Float heading = (float) computeHeading(cur, nxt);
+                                        CameraPosition pos = new CameraPosition.Builder().target(cur).bearing(heading).tilt(45).zoom(16).build();
+                                        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(pos));
+                                        if (oldMarker != null) {
+                                            oldMarker.remove();
+                                        }
+
+                                        MarkerOptions newMarker = new MarkerOptions()
+                                                .position(cur)
+                                                .title("Current Position")
+                                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.navi))
+                                                .anchor(0.5f, 3.0f / 5.0f)
+                                                .rotation(1.0f / heading);
+                                        oldMarker = googleMap.addMarker(newMarker);
+                                    } else {
+                                        t.cancel();
                                     }
-
-                                    MarkerOptions newMarker = new MarkerOptions()
-                                            .position(cur)
-                                            .title("Current Position")
-                                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.navi))
-                                            .anchor(0.5f, 3.0f / 5.0f)
-                                            .rotation(1.0f/heading);
-                                    oldMarker = googleMap.addMarker(newMarker);
                                 }
-                                else
-                                {
-                                    t.cancel();
-                                }
-                            }
-                        });
+                            });
+                        }
                     }
                 }, 1000, 200);
             }
